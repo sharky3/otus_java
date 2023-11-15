c00a8a5d138ab31c06572ddbed82fbea4e06cbe5
ZM <thebeliever3@gmail.com>
2023-11-15 18:47:29 +0400

    OTUS-HW01 Created gradle project with fat jar

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+rootProject.name = "otus_java"
+include("hw01-gradle")
+
+pluginManagement {
+    val jgitverPluginVersion: String by settings
+    val springPluginVersion: String by settings
+    val springBootPluginVersion: String by settings
+    val shadowJarPluginVersion: String by settings
+    val sonarPluginVersion: String by settings
+    val spotlessPluginVersion: String by settings
+
+    plugins {
+        id("fr.brouillard.oss.gradle.jgitver") version jgitverPluginVersion
+        id("io.spring.dependency-management") version springPluginVersion
+        id("org.springframework.boot") version springBootPluginVersion
+        id("com.github.johnrengelman.shadow") version shadowJarPluginVersion
+        id("name.remal.sonarlint") version sonarPluginVersion
+        id("com.diffplug.spotless") version spotlessPluginVersion
+    }
+}
